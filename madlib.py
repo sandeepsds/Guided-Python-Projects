c6beb729f3daff0adb5b014d0f9dfc35a51c544b
Sandeep Sahani <thecslad@gmail.com>
2021-11-24 19:58:46 +0530

    This is my first python project

---
+++ madlib.py
@@ -0,0 +1,10 @@
+#Madlib
+#Ways to concatenae strings
+programming_language = input("Enter a programming language: ")
+hobby = input("Enter your hobby: ")
+super_hero = input("Enter the name of a super hero: ")
+
+madlib = f"""The best way to start learning to code is with {programming_language}! 
+I like to {hobby} during my free time. I like {super_hero} a lot. """
+
+print(madlib)
